d60729173d33454d514afd2136c2bb58a8fb7237
sgyhmsly <84810905@qq.com>
2016-12-30 13:15:05 +0800

    testing for getFileEncode

---
+++ tests/CommonUtil/FileEncodingTest.java
@@ -0,0 +1,21 @@
+package CommonUtil;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by DT173 on 2016/12/30.
+ */
+public class FileEncodingTest
+{
+    @Test
+    public void getFileEncode() throws Exception
+    {
+//        String encoding = FileEncoding.getFileEncode(new File("C:\\TEMP\\TEST.TXT"));
+//        return;
+    }
+
+}
