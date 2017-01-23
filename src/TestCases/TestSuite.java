fdb3b433e4e964ada97ea3653b52a196ab7e0b20
sgyhmsly <84810905@qq.com>
2017-01-23 22:26:24 +0800

    replace TestGroup as TestSuite

--- src/TestCases/TestSuite.java
+++ src/TestCases/TestSuite.java
@@ -6,14 +6,12 @@ import ORTEExceptions.StepFileNotNullException;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 /**
- * Created by JJMM on 2017/1/14.
+ * Created by JJMM on 2017/1/23.
  */
-public class TestGroup extends TestComposite
+public class TestSuite extends TestComposite
 {
-
-    public TestGroup(final File groupName)throws StepFileNotNullException,FileNotFoundException
+    public TestSuite(final File groupName)throws StepFileNotNullException,FileNotFoundException
     {
         super(groupName);
     }
@@ -22,7 +20,7 @@ public class TestGroup extends TestComposite
     {
         if (tElement instanceof AbstractStep)
             throw new LeafStepException("LeafStep can not be added into test group");
-        if (tElement instanceof TestCase|| tElement instanceof TestGroup)
+        if (tElement instanceof TestCase|| tElement instanceof TestSuite)
             components.add(tElement);
     }
 
@@ -31,9 +29,7 @@ public class TestGroup extends TestComposite
     {
         if (tElement instanceof AbstractStep)
             return;
-        if (tElement instanceof TestCase||tElement instanceof TestGroup)
+        if (tElement instanceof TestCase||tElement instanceof TestSuite)
             components.remove(tElement);
     }
-
-
 }
